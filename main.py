f39216bbd5d91a0a0894100c05d682329f49c28a
Guilherme Guerreiro <guiguerreiro10@hotmail.com>
2020-09-13 22:43:43 +0100

    Possible moves are showing and game is playable!

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
 from checkers.board import Board
+from checkers.game import Game
 
 FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('checkers')
+
 
 def get_row_col_from_mouse(pos):
     x, y = pos
@@ -14,14 +16,18 @@ def get_row_col_from_mouse(pos):
 
     return row, col
 
+
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
-        
+
+        if game.winner != None:
+            print(game.winner)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -29,12 +35,12 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
-                piece = board.get_piece(row, col)
+                game.select(row, col)
                 # board.move(piece, 4, 3)
-        
-        board.draw(WIN)
-        pygame.display.update()
+
+        game.update()
 
     pygame.quit()
+
 
 main()
